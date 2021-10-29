8df2b1c224653076dd65c1f7f7e8391ffb65fc91
anitkumarsingh <anikumarsingh99@gmail.com>
2021-10-29 10:07:45 +0530

    rendering recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes : Recipe[] = [ new Recipe('Curry Recipes',`
+  curry recipes category consists of both indian curry recipes and a few world recipes as well. the indian curry collection includes curry or gravy recipes from all over india and stew recipes.
+  curry can be made with vegetables, legumes, lentils and this category shares all the famous and even some infamous vegetarian curry recipes. also includes popular vegetable curry recipes from both north indian and south indian cuisine.`,'https://www.vegrecipesofindia.com/wp-content/uploads/2013/11/instant-pot-chana-masala-recipe-3-280x280.jpg')]
   constructor() { }
 
   ngOnInit(): void {
